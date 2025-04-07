48b871b6c5f33fbff34bc7e7697fb5db72398b00
hemantshelkar <97467301+hemantshelkar@users.noreply.github.com>
2025-04-07 13:48:01 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=12345;
+let accountEmail="hms@gmail.com";
+var accountName="Hemant";
+accountCity="Indore";
+
+/*accountId=2121212;  
+can not re-intialize the value of const
+console.log(accountId);*/
+
+accountEmail="hemant@gmail.com";
+accountName="Hemant Shelkar";
+accountCity="Pune";
+
+console.table([accountId,accountEmail,accountName,accountCity]);
+
