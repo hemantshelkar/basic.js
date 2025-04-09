b0aed3211434b2e84db87298563f59348be38755
hemantshelkar <97467301+hemantshelkar@users.noreply.github.com>
2025-04-09 11:29:12 +0000

    typeconversion

---
+++ 01_basics/01_typeconversion.js
@@ -0,0 +1,46 @@
+let score=33
+
+console.log(typeof score)//score as variable
+console.log(typeof (score))
+
+let Name="hemant";
+console.log(typeof Name);
+let isName=Number(Name);
+console.log(typeof isName);
+console.log(isName);
+
+let isBoolean=false;
+console.log(typeof isBoolean);
+let booleanisBoolean=Number(isBoolean)
+console.log(booleanisBoolean);
+console.log(typeof booleanisBoolean);
+
+let State="Hemant"
+let isState=Number(State);
+console.log(isState);
+console.log(typeof isState);
+let isBool=Boolean(State);
+console.log(isBool);    
+console.log(typeof isBool);
+
+
+let No=55
+let isNo=String(No);
+console.log(typeof isNo);
+console.log(isNo);
+
+let s="";
+let iss=Boolean(s);
+console.log(iss);
+
+
+
+// string to Number     ==>> NaN
+// false to number      ==>> 0
+// true to number       ==>> 1
+// string to boolean    ==>> true  if "abc" filled whitespaces is also considered as a filled string
+// string to boolean    ==>> false if "" empty
+//  
+
+
+
