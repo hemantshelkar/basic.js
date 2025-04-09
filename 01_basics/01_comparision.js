3ee3070b53a7a98d3bfa4adc008209402d291c18
hemantshelkar <97467301+hemantshelkar@users.noreply.github.com>
2025-04-09 13:00:30 +0000

    comparision

---
+++ 01_basics/01_comparision.js
@@ -0,0 +1,40 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);          conversion is performed internally
+// console.log(2<"1");
+// console.log(2=="2");
+
+// console.log(null<=1);
+// console.log(null>=0);
+// console.log(null==0);
+// console.log(null==1);
+// console.log(null<1);
+// console.log(null>0);
+
+
+// console.log(undefined<=1);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+// console.log(undefined==1);
+// console.log(undefined<1);
+// console.log(undefined>0);
+
+// === Strict comparision
+//== only compare on the basis of value but,
+// === compares on the basis of value as well as datatype of the value
+
+console.log("2"==2);//true;
+console.log("2"===2); //false
+
+
+
+
+
+
+
+
