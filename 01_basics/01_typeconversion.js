c7963541c802fc4f0a7515267b0953afffa9baa6
hemantshelkar <97467301+hemantshelkar@users.noreply.github.com>
2025-04-09 12:20:31 +0000

    operations

--- 01_basics/01_typeconversion.js
+++ 01_basics/01_typeconversion.js
@@ -1,37 +1,37 @@
 let score=33
 
-console.log(typeof score)//score as variable
-console.log(typeof (score))
+// console.log(typeof score)//score as variable
+// console.log(typeof (score))
 
 let Name="hemant";
-console.log(typeof Name);
+// console.log(typeof Name);
 let isName=Number(Name);
-console.log(typeof isName);
-console.log(isName);
+// console.log(typeof isName);
+// console.log(isName);
 
 let isBoolean=false;
-console.log(typeof isBoolean);
+// console.log(typeof isBoolean);
 let booleanisBoolean=Number(isBoolean)
-console.log(booleanisBoolean);
-console.log(typeof booleanisBoolean);
+// console.log(booleanisBoolean);
+// console.log(typeof booleanisBoolean);
 
 let State="Hemant"
 let isState=Number(State);
-console.log(isState);
-console.log(typeof isState);
+// console.log(isState);
+// console.log(typeof isState);
 let isBool=Boolean(State);
-console.log(isBool);    
-console.log(typeof isBool);
+// console.log(isBool);    
+// console.log(typeof isBool);
 
 
 let No=55
 let isNo=String(No);
-console.log(typeof isNo);
-console.log(isNo);
+// console.log(typeof isNo);
+// console.log(isNo);
 
 let s="";
 let iss=Boolean(s);
-console.log(iss);
+// console.log(iss);
 
 
 
@@ -41,6 +41,48 @@ console.log(iss);
 // string to boolean    ==>> true  if "abc" filled whitespaces is also considered as a filled string
 // string to boolean    ==>> false if "" empty
 //  
+
+
+
+// ************************************Operation*************************************
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1=2+2;
+let str2=4;
+let str3=str1+str2;
+console.log(str3);
+console.log("str1"+" str2");
+// // console.log(str1);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+let str4="Hemant";
+console.log(+str4);
+let a=true;
+console.log(+a);
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+console.log(gameCounter++);
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
 
