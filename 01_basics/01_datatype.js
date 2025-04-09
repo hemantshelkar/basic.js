4cbd93f665fc20fb35079de62fc16ff19d2f3580
hemantshelkar <97467301+hemantshelkar@users.noreply.github.com>
2025-04-09 10:34:55 +0000

    datatypes done

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,37 @@
+"use strict"// treat all js code as newer version 
+
+//DataType
+
+//alert(3+3)//error because we use node.js not browser
+
+/*console.log(3+   => readiability should be high
+    3)   <= avoid like this..
+
+console.log("hemant")*/
+
+let name="Hemant";
+let age=18;
+let loggedIn=false;
+let state =null;
+
+//String    => "statements written in double quotes considered as a string"
+//number    => 2 to power of 53
+//bigint    => used to store large number mostly used in large computational software like payments and stock market
+//null      => it is a single value or standalone value
+//undefined => it is also a single value or value is not defined yet.
+//symbol    => unique values
+
+
+//Object
+
+console.log("output =",typeof "Hemant");
+console.log("output =",typeof undefined);
+console.log("output =",typeof null);
+console.log("output =",typeof 3);
+console.log("output =",typeof true);
+console.log("output =",typeof 3.2);
+
+
+
+
+
