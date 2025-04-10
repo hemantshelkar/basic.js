31da8aaf9184e970410057ca94ed757b986f4343
hemantshelkar <97467301+hemantshelkar@users.noreply.github.com>
2025-04-10 13:28:21 +0000

    memory

--- 01_basics/01_dtSummary.js
+++ 01_basics/01_dtSummary.js
@@ -12,7 +12,7 @@ const userName="Ram";
 // console.log(typeof userName);
 const id=Symbol('123');
 const newId=Symbol('123');
-console.log(id===newId);     //symbols are always unique no matter they contain same value but variable considered the value always unique
+// console.log(id===newId);     //symbols are always unique no matter they contain same value but variable considered the value always unique
 
 //2-> reference (non-premitive datatypes)
 
@@ -24,10 +24,37 @@ let myObj={
     age:24,
     temp:null
 }
-console.log(myObj);
+// console.log(myObj);
 
 
 const myFunction=function(){
-    console.log("Hello World");
+    // console.log("Hello World");
     
 }
+
+//**************************************************************************************** */
+
+// Memory in js 
+// stack and heap memory
+
+// premitive datatypes are store in stack memory
+// and 
+//reference(non-premitive) data types are store in heap memory
+
+// variable are always use stack memory
+
+let channelName="Hemantshelkar";
+let newChannel=channelName;
+channelName="Natural Coder";
+
+console.log(newChannel);
+console.log();
+
+let userOne={
+    Email:"abc@gmail.com",
+    upi:"abc@ybl",
+}
+let userTwo=userOne;
+
+userOne.Email="oooooo@gmail.com"
+
